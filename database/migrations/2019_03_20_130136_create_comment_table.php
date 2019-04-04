f898face7cfb9290c0f0f78d01aa573f65870084
nguyenthanhtuan15 <nguyenthanhtuan.15it@gmail.com>
2019-04-05 00:18:03 +0700

    fix

--- database/migrations/2019_03_20_130136_create_comment_table.php
+++ database/migrations/2019_03_20_130136_create_comment_table.php
@@ -1,34 +1,31 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateCommentTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('comment', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->bigInteger('tour_id')->unsigned();
-            $table->bigInteger('customer_id')->unsigned();
-            $table->string('content');
-            $table->timestamps();
-        });
-    }
+class CreateCommentTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('comment', function (Blueprint $table) {
+			$table->bigIncrements('id');
+			$table->bigInteger('tour_id')->unsigned();
+			$table->bigInteger('user_id')->unsigned();
+			$table->string('content');
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('comment');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('comment');
+	}
 }
