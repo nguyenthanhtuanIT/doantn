b35135799f7b7f74b6b86c9992aff33b29528e1c
nguyenthanhtuan15 <nguyenthanhtuan.15it@gmail.com>
2019-04-11 10:20:25 +0700

    v3.1

--- app/Http/Controllers/Commentcontroller.php
+++ app/Http/Controllers/Commentcontroller.php
@@ -45,9 +45,7 @@ class Commentcontroller extends Controller {
 		$com = new Comment;
 		$com->del($id);
 		$comment = $com->getCommentbyId($id);
-		foreach ($comment as $com) {
-			return redirect('blog/' . $com->tour_id);
-		}
+		return redirect('blog/' . $comment->tour_id);
 
 	}
 }
